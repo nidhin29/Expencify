88207f43843004aef228f52b3b8350c2381777c0
Nidhin Ninan <nidhinvninan29@gmail.com>
2026-06-21 23:36:45 +0530

    chore: set upgrader check frequency cache duration to 4 hours

--- lib/upgrader_config.dart
+++ lib/upgrader_config.dart
@@ -3,4 +3,5 @@ import 'package:upgrader/upgrader.dart';
 final Upgrader appUpgrader = Upgrader(
   debugDisplayAlways: false,
   debugLogging: false,
+  durationUntilAlertAgain: const Duration(hours: 4),
 );
